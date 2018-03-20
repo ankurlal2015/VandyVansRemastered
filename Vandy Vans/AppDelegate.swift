34af9514f4d87ee61df10f03fb12df0620d4b847
Ankur Lal <ankur.lal@vanderbilt.edu>
2018-03-20 10:31:24 -0500

    map

--- Vandy Vans/AppDelegate.swift
+++ Vandy Vans/AppDelegate.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        //Get Maps
+        GMSServices.provideAPIKey("AIzaSyC7ax-EhQHOLWrRh1bt5AeE80lkE0bU4oA")
         // Override point for customization after application launch.
         return true
     }
@@ -40,7 +43,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
 
 }
 
